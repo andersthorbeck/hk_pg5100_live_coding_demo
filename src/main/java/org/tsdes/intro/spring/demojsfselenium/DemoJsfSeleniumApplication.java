29583197c8191831f59d0e340991a6f3383356ef
Anders Rabo Thorbeck <andtho@knowit.no>
2021-04-08 16:28:15 +0200

    First commit

---
+++ src/main/java/org/tsdes/intro/spring/demojsfselenium/DemoJsfSeleniumApplication.java
@@ -0,0 +1,13 @@
+package org.tsdes.intro.spring.demojsfselenium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJsfSeleniumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoJsfSeleniumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/tsdes/intro/spring/demojsfselenium/DemoJsfSeleniumApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tsdes.intro.spring.demojsfselenium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJsfSeleniumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
