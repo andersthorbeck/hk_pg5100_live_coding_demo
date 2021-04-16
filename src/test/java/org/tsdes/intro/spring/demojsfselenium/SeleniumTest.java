83f1545b75fae4e951776ff66aaf8f5f387c2c75
Anders Rabo Thorbeck <andtho@knowit.no>
2021-04-16 19:08:40 +0200

    Removes unused imports

--- src/test/java/org/tsdes/intro/spring/demojsfselenium/SeleniumTest.java
+++ src/test/java/org/tsdes/intro/spring/demojsfselenium/SeleniumTest.java
@@ -1,9 +1,7 @@
 package org.tsdes.intro.spring.demojsfselenium;
 
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 
