b4774bbd407484bfa092edd9ea4a59ed084cbdab
Anders Rabo Thorbeck <andtho@knowit.no>
2021-04-23 10:13:36 +0200

    Removes unused imports

--- src/test/java/org/tsdes/intro/spring/demojsfselenium/CounterServiceTest.java
+++ src/test/java/org/tsdes/intro/spring/demojsfselenium/CounterServiceTest.java
@@ -1,15 +1,11 @@
 package org.tsdes.intro.spring.demojsfselenium;
 
-import org.hamcrest.MatcherAssert;
-import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.greaterThanOrEqualTo;
 import static org.hamcrest.Matchers.is;
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class CounterServiceTest {
